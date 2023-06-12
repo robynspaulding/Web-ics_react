425e989f2f5727d13db4ca4a8d5c48da1f73a5f4
RobynSpaulding <robynm27@gmail.com>
2023-06-11 20:14:59 -0700

    adds test array to content file to set props that can be accessed in the ComicsIndex file and mapped over, that works, tried to implement axios.get to render props as the data from my backend but in the ComicsIndex the .map was not working so I need to add a prop-type or something to insure that the data being returned is an array so taht I can map over it

--- src/ComicsIndex.jsx
+++ src/ComicsIndex.jsx
@@ -1,7 +1,13 @@
-export function ComicsIndex() {
+export function ComicsIndex(props) {
   return (
     <div>
       <h1>All Comics!</h1>
+      {props.comics.map((comic) => (
+        <div key={comic.id}>
+          <h2>{comic.title}</h2>
+          <img src={comic.image} width="500" height="300" />
+        </div>
+      ))}
     </div>
   );
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { ComicsIndex } from "./ComicsIndex";
 import { Login } from "./Login";
 import { Signup } from "./Signup";
 
 export function Content() {
+  // const [comics, setComics] = useState([]);
+
+  const handleIndexComics = () => {
+    console.log("handleIndexComics");
+    axios.get("http://localhost:3000/comics.json").then((response) => {
+      console.log(response.date);
+      setComics(response.date);
+    });
+  };
+
+  const comics = [
+    { id: 1, title: "Test", image: "https://i.redd.it/w3kr4m2fi3111.png" },
+    { id: 2, title: "Test 2", image: "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_960_720.png" },
+  ];
+
+  useEffect(handleIndexComics, []);
+
   return (
     <div>
       <h1>Welcome to Web-ics!</h1>
       <Signup />
       <Login />
-      <ComicsIndex />
+      <ComicsIndex comics={comics} />
     </div>
   );
 }
